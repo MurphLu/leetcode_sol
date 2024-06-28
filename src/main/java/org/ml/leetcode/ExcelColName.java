486651e93bda22863b0d92b48ea68bd89d479a1f
murph.lu <murph.lu@homecreditcfc.cn>
2024-06-28 15:28:13 +0800

    add leetcode

--- src/main/java/org/ml/leetcode/ExcelColName.java
+++ src/main/java/org/ml/leetcode/ExcelColName.java
@@ -2,18 +2,20 @@ package org.ml.leetcode;
 
 /**
  * 给你一个整数 columnNumber ，返回它在 Excel 表中相对应的列名称。
+ * 这是一道从 1 开始的的 26 进制转换题。
  */
 public class ExcelColName {
     public static void main(String[] args) {
         System.out.println(new ExcelColName().convertToTitle(27));
     }
     public String convertToTitle(int columnNumber) {
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         while (columnNumber > 0) {
-            int a0 = (columnNumber - 1) % 26 + 1;
-            sb.append((char)(a0 - 1 + 'A'));
-            columnNumber = (columnNumber - a0) / 26;
+            columnNumber--;
+            sb.append((char)(columnNumber % 26 + 'A'));
+            columnNumber /= 26;
         }
-        return sb.reverse().toString();
+        sb.reverse();
+        return sb.toString();
     }
 }
