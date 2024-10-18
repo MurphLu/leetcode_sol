243e26d85dee288fe2ec05148339103933cf1fff
murph.lu <murph.lu@homecreditcfc.cn>
2024-10-18 14:25:02 +0800

    add leetcode daily

--- src/main/java/org/ml/leetcode/daily/MinOperations.java
+++ src/main/java/org/ml/leetcode/daily/MinOperations.java
@@ -1,5 +1,15 @@
 package org.ml.leetcode.daily;
 
+/**
+ * 给你一个二进制数组 nums 。
+ *
+ * 你可以对数组执行以下操作 任意 次（也可以 0 次）：
+ *
+ * 选择数组中 任意连续 3 个元素，并将它们 全部反转 。
+ * 反转 一个元素指的是将它的值从 0 变 1 ，或者从 1 变 0 。
+ *
+ * 请你返回将 nums 中所有元素变为 1 的 最少 操作次数。如果无法全部变成 1 ，返回 -1 。
+ */
 public class MinOperations {
     public static void main(String[] args) {
 
