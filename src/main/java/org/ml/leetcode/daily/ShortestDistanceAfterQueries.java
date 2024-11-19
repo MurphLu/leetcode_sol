4701ef3246e63807f86bbb24b5fee7cd4ed65c86
nurphlu <murphlu@163.com>
2024-11-19 13:58:55 +0800

    leetcode daily

--- src/main/java/org/ml/leetcode/daily/ShortestDistanceAfterQueries.java
+++ src/main/java/org/ml/leetcode/daily/ShortestDistanceAfterQueries.java
@@ -2,6 +2,23 @@ package org.ml.leetcode.daily;
 
 import java.util.*;
 
+/**
+ * 给你一个整数 n 和一个二维整数数组 queries。
+ *
+ * 有 n 个城市，编号从 0 到 n - 1。初始时，每个城市 i 都有一条单向道路通往城市 i + 1（ 0 <= i < n - 1）。
+ *
+ * queries[i] = [ui, vi] 表示新建一条从城市 ui 到城市 vi 的单向道路。每次查询后，你需要找到从城市 0 到城市 n - 1 的最短路径的长度。
+ *
+ * 返回一个数组 answer，对于范围 [0, queries.length - 1] 中的每个 i，answer[i] 是处理完前 i + 1 个查询后，从城市 0 到城市 n - 1 的最短路径的长度。
+ *
+ *
+ * 3 <= n <= 500
+ * 1 <= queries.length <= 500
+ * queries[i].length == 2
+ * 0 <= queries[i][0] < queries[i][1] < n
+ * 1 < queries[i][1] - queries[i][0]
+ * 查询中没有重复的道路。
+ */
 public class ShortestDistanceAfterQueries {
     public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
         List<Integer>[] list = new List[n];
