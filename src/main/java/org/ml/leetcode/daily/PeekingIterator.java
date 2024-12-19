88685c154aad7b64356b659f5cf676890010110f
murphlu <murphlu@163.com>
2024-12-19 14:36:36 +0800

    leetcode daily

---
+++ src/main/java/org/ml/leetcode/daily/PeekingIterator.java
@@ -0,0 +1,39 @@
+package org.ml.leetcode.daily;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class PeekingIterator implements Iterator<Integer>{
+    List<Integer> list;
+    int index;
+    public PeekingIterator(Iterator<Integer> iterator) {
+        // initialize any member here.
+        list = new ArrayList<>();
+        while (iterator.hasNext()){
+            list.add(iterator.next());
+        }
+        index = 0;
+    }
+
+    // Returns the next element in the iteration without advancing the iterator.
+    public Integer peek() {
+        if (index < list.size()){
+            return list.get(index);
+        }else {
+            return null;
+        }
+    }
+
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    @Override
+    public Integer next() {
+        return list.get(index++);
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < list.size();
+    }
+}
