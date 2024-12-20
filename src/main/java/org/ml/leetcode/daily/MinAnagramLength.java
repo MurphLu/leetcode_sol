5f3b1709d00a0b28a11e9ffee83e1d3d1beef0e0
murphlu <murphlu@163.com>
2024-12-20 11:32:33 +0800

    leetcode daily

--- src/main/java/org/ml/leetcode/daily/MinAnagramLength.java
+++ src/main/java/org/ml/leetcode/daily/MinAnagramLength.java
@@ -3,6 +3,33 @@ package org.ml.leetcode.daily;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 给你一个字符串 s ，它由某个字符串 t 和若干 t 的 同位字符串 连接而成。
+ *
+ * 请你返回字符串 t 的 最小 可能长度。
+ *
+ * 同位字符串 指的是重新排列一个单词得到的另外一个字符串，原来字符串中的每个字符在新字符串中都恰好只使用一次。
+ *
+ * 示例 1：
+ *
+ * 输入：s = "abba"
+ *
+ * 输出：2
+ *
+ * 解释：
+ *
+ * 一个可能的字符串 t 为 "ba" 。
+ *
+ * 示例 2：
+ *
+ * 输入：s = "cdef"
+ *
+ * 输出：4
+ *
+ * 解释：
+ *
+ * 一个可能的字符串 t 为 "cdef" ，注意 t 可能等于 s 。
+ */
 public class MinAnagramLength {
     public static void main(String[] args) {
         new MinAnagramLength().minAnagramLength("aabbabab");
